ad2f9f6dfa8b7ceed55f9f34653bfa6a7c14fc90
Trevor Davis <davist11@gmail.com>
2015-01-14 11:26:30 -0500

    use consistent tabbing

--- variables/InlinVariable.php
+++ variables/InlinVariable.php
@@ -5,16 +5,16 @@ class InlinVariable
 {
 	public function er($fileName)
 	{
-    $documentRoot = craft()->inlin->getSetting('inlinPublicRoot')!=null ? craft()->inlin->getSetting('inlinPublicRoot') : $_SERVER['DOCUMENT_ROOT'];
-    $filePath = $documentRoot . $fileName;
-    
-    if ($fileName != '' && file_exists($filePath)) {
-      
-      $content = file_get_contents($filePath);
-      return $content;
-      
-    } else {
-      return '';
-    }
+		$documentRoot = craft()->inlin->getSetting('inlinPublicRoot')!=null ? craft()->inlin->getSetting('inlinPublicRoot') : $_SERVER['DOCUMENT_ROOT'];
+		$filePath = $documentRoot . $fileName;
+
+		if ($fileName != '' && file_exists($filePath)) {
+
+			$content = file_get_contents($filePath);
+			return $content;
+
+		} else {
+			return '';
+		}
 	}
 }
